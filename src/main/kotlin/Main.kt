3689f1a12cdde8b97abf35833d71101a40022037
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-09-05 19:38:03 -0300

    feat: Adicionado variável para armazenar a frase que o usuário digitar

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+
+fun main() {
+    println("CONTADOR DE PALAVRAS EM UMA FRASE")
+    println("Por favor, digite uma frase: ")
+    val fraseDigitadaPeloUsuario = readln()
+    println("Frase: $fraseDigitadaPeloUsuario")
+}
